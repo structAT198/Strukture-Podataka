6770dc829eb817799666bffdc92de2feaf08ba79
structAT198 <73576982+structAT198@users.noreply.github.com>
2020-11-05 19:27:51 +0100

    Vjezba 4 Partial

---
+++ Vjezba_4.cpp
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define IME_PREZIME_MAX 50
+
+typedef struct Polinom
+{
+	int coefficient;
+	int exponent;
+	struct Polinom* next;
+}Polinom;
+
+Polinom* Nova_Polinom(int coefficient, int exponent)
+{
+	Polinom* novi = (Polinom*) malloc(sizeof(Polinom));
+	novi->coefficient = coefficient;
+	novi->exponent = exponent;
+	novi->next = NULL;
+	return novi;
+}
+
+void Na_Pocetak(Polinom* dummy, Polinom* nova)
+{
+	nova->next = dummy->next;
+	dummy->next = nova;
+}
+
+void Dodaj_Nakon(Polinom* dummy, Polinom* nakon_mene, Polinom* nova)
+{
+	Polinom* tmp = dummy->next;
+	while(tmp != nakon_mene)
+	{
+		tmp = tmp->next;
+	}
+	Polinom* od_nakon = tmp->next;
+	tmp->next = nova;
+	nova->next = od_nakon;
+}
+
+void Dodaj_Ispred(Polinom* dummy, Polinom* prije_mene, Polinom* nova)
+{
+	Polinom* tmp = dummy->next;
+	while(tmp->next != prije_mene)
+	{
+		tmp = tmp->next;
+	}
+	Polinom* od_nakon = tmp->next;
+	tmp->next = nova;
+	nova->next = od_nakon;
+}
+
+void Na_Kraj(Polinom* dummy, Polinom* nova)
+{
+	Polinom* tmp = NULL;
+	tmp = dummy->next;
+	while(tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = nova;
+}
+
+void Ispis_Liste(Polinom* dummy)
+{
+	Polinom* tmp = dummy->next;
+	while(tmp != NULL)
+	{
+		printf("%dX^%d ", tmp->coefficient, tmp->exponent);
+		tmp = tmp->next;
+	}
+}
+
+void Unesi_Sortirano(Polinom* dummy, Polinom* novi)
+{
+	//FIXME, NE RADI (JOS!);
+	Polinom* tmp = dummy->next;
+	while(tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	if(tmp->exponent < novi->exponent && tmp != NULL)
+	{
+		tmp->next = novi;
+	}
+	else if(tmp->exponent > novi->exponent && tmp != NULL)
+	{
+		Polinom* dupli_tmp = tmp;
+		tmp = novi;
+		novi->next = dupli_tmp->next;
+	}
+}
+
+void Ucitaj_Polinom(const char* filename, Polinom* dummy)
+{
+	FILE* polinom = fopen(filename, "r");
+	if(polinom == NULL)
+	{
+		printf("\nFATAL ERROR! Nema polinoma!");
+	}
+	else
+	{
+		Polinom* temp = (Polinom*) malloc(sizeof(Polinom));
+		int tmp = 0;
+		while(feof(polinom) != 1)
+		{
+			tmp = fscanf(polinom, "%d %d", & temp->coefficient, & temp->exponent);
+			Unesi_Sortirano(dummy, temp);
+		}
+	}
+}
+
+int main()
+{
+	Polinom prvi;
+	Polinom drugi;
+	prvi.next = NULL;
+	drugi.next = NULL;
+	Ucitaj_Polinom("Polinom_1.txt", & prvi);
+	Ispis_Liste(& prvi);
+	Ucitaj_Polinom("Polinom_2.txt", & drugi);
+	Ispis_Liste(&drugi);
+	system("pause");
+	return 0;
+}
